c2598ae67d84b76a5d1d6a54c2e73f76e978a83c
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-12 22:42:03 +0545

    array manipulation

--- index2.js
+++ index2.js
@@ -1,13 +1,15 @@
-var x=8;
-{
-    var x=9;
-    // console.log(x);
-}
-console.log(x);
+let a = ["He","She"];
+// Adds at last
+// a.push("They");
+// Adds at first
+// a.unshift("They");
+// Remove last elemnt
+// a.pop();
+// Remove 1st element
+// a.shift();
+// Joining elemets
+// let b = a.join('-');
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
 
-let y=8;
-{
-    let y=9;
-    // console.log(x);
-}
-console.log(y);
+// At position 2, remove 2 items: 
+// fruits.splice(2, 1,"Hello");
